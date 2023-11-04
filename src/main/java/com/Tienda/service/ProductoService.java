4e67f9fb17a374aa5ea67fdbb163ac4db98c8bbe
WendyBadilla <117774092+WendyBadilla@users.noreply.github.com>
2023-11-03 20:11:45 -0600

    Se agrego la vista de productos

---
+++ src/main/java/com/Tienda/service/ProductoService.java
@@ -0,0 +1,18 @@
+
+package com.Tienda.service;
+
+import com.Tienda.domain.Producto;
+import java.util.List;
+
+
+public interface ProductoService {
+    
+    public List<Producto> getProductos(boolean activos);
+    
+    public Producto getProducto(Producto producto);
+    
+    public void save(Producto producto);
+    
+    public void delete(Producto producto);
+    
+}

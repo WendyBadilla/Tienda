b1f17889f55f197ffad543c6686035dec824f65f
WendyBadilla <117774092+WendyBadilla@users.noreply.github.com>
2023-11-14 21:29:04 -0600

    Semana 10, usuarios y roles

---
+++ src/main/java/com/Tienda/domain/Usuario.java
@@ -0,0 +1,45 @@
+
+package com.Tienda.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data   //Para la creación automatica de los set y get de los atributos
+@Entity
+@Table(name="usuario")
+public class Usuario implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+    
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String password;
+    @NotEmpty
+    private String nombre;
+    @NotEmpty
+    private String apellidos;
+    private String correo;
+    private String telefono;
+    private String rutaImagen;
+    private boolean activo;
+    
+    @OneToMany
+    @JoinColumn(name = "id_usuario")
+    List<Rol> roles;
+}
